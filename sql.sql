6a1eecae375d9bd5e7f8a50d16aa72c1dd888719
Tejaswini reddy <tejaswinireddydonthireddy@gmail.com>
2026-03-04 13:02:45 +0530

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -78,18 +78,37 @@ SELECT Account_Type,
    ;
 -- 9. HIGH-RISK STATE + TYPE CROSS-ANALYSIS
 SELECT
+    f.State,
+    f.Transaction_Type,
+    COUNT(*) AS Total,
+    SUM(f.Is_Fraud) AS Fraud_Count,
+    ROUND(SUM(f.Is_Fraud) * 100.0 / COUNT(*), 2) AS Fraud_Rate_Pct
+FROM fraud f
+
+JOIN (
+    SELECT State
+    FROM fraud
+    GROUP BY State
+    ORDER BY SUM(Is_Fraud) DESC
+    LIMIT 5
+) top_states
+ON f.State = top_states.State
+
+GROUP BY f.State, f.Transaction_Type
+ORDER BY f.State, Fraud_Rate_Pct DESC;
+
+-- 10. NIGHT-TIME FRAUD DEEP DIVE (Hours 22–4)
+SELECT
+Transaction_time,
     State,
-    Transaction_Type,
     COUNT(*) AS Total,
     SUM(Is_Fraud) AS Fraud_Count,
     ROUND(SUM(Is_Fraud) * 100.0 / COUNT(*), 2) AS Fraud_Rate_Pct
 FROM fraud
-WHERE State IN (
-    SELECT State FROM transactions GROUP BY State
-    ORDER BY SUM(Is_Fraud) DESC LIMIT 5
-)
-GROUP BY State, Transaction_Type
-ORDER BY State, Fraud_Rate_Pct DESC;
+WHERE Transaction_time  IN (22, 23, 0, 1, 2, 3, 4)
+GROUP BY Transaction_time , State
+ORDER BY Fraud_Rate_Pct DESC
+;
 
 -- 10. NIGHT-TIME FRAUD DEEP DIVE (Hours 22–4)
 SELECT
@@ -105,16 +124,30 @@ ORDER BY Fraud_Rate_Pct DESC
 LIMIT 20;
 
 -- 11. AMOUNT BUCKET RISK ANALYSIS
-SELECT
-   Transaction_Amount ,
+ELECT
+    CASE
+        WHEN Transaction_Amount < 1000 THEN '<1K'
+        WHEN Transaction_Amount >= 1000 AND Transaction_Amount < 5000 THEN '1K-5K'
+        WHEN Transaction_Amount >= 5000 AND Transaction_Amount < 20000 THEN '5K-20K'
+        WHEN Transaction_Amount >= 20000 AND Transaction_Amount < 50000 THEN '20K-50K'
+        ELSE '50K+'
+    END AS Amount_Bucket,
+
     COUNT(*) AS Total,
     SUM(Is_Fraud) AS Fraud_Count,
     ROUND(SUM(Is_Fraud) * 100.0 / COUNT(*), 2) AS Fraud_Rate_Pct
-FROM transactions
-GROUP BY Transaction_Amount
-ORDER BY case  Transaction_Amount
-    WHEN '<1K' THEN 1 WHEN '1K-5K' THEN 2 WHEN '5K-20K' THEN 3
-    WHEN '20K-50K' THEN 4 WHEN '50K+' THEN 5 END;
+
+FROM fraud
+GROUP BY Amount_Bucket
+
+ORDER BY 
+    CASE Amount_Bucket
+        WHEN '<1K' THEN 1
+        WHEN '1K-5K' THEN 2
+        WHEN '5K-20K' THEN 3
+        WHEN '20K-50K' THEN 4
+        WHEN '50K+' THEN 5
+    END;
 
 -- 12. TOP 10 HIGHEST FRAUD-RATE STATE + CATEGORY COMBOS
 SELECT
@@ -125,7 +158,7 @@ SELECT
     ROUND(SUM(Is_Fraud) * 100.0 / COUNT(*), 2) AS Fraud_Rate_Pct
 FROM fraud
 GROUP BY State, Merchant_Category
-HAVING Total >= 50
+HAVING Total >= 5
 ORDER BY Fraud_Rate_Pct DESC
 LIMIT 10;
 
